b6583e211e6848ca028ba13a2d963d0127197fbe
Tin Tran <tinhardware@gmail.com>
2022-06-11 07:09:26 +0700

    Update UI

--- Notes WatchKit Extension/View/ContentView.swift
+++ Notes WatchKit Extension/View/ContentView.swift
@@ -8,12 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - PROPERTY
+
+    @State private var notes: [Note] = [Note]()
+    @State private var text: String = ""
+
+    // MARK: - FUNCTION
+    func save() {
+        dump(notes)
+    }
+
+    // MARK: - BODY
     var body: some View {
-        Text("Hello, World!")
-            .padding()
+        VStack {
+            HStack(alignment: .center, spacing: 6) {
+                TextField("Add New Note", text: $text)
+
+                Button {
+                    guard text.isEmpty == false else { return }
+                    let note = Note(id: UUID(), text: text)
+                    notes.append(note)
+                    text = ""
+                    save()
+                } label: {
+                    Image(systemName: "plus.circle")
+                        .font(.system(size: 42, weight: .semibold))
+                }
+                .fixedSize()
+                .buttonStyle(PlainButtonStyle())
+                .foregroundColor(.accentColor)
+            } //: HSTACK
+
+            Spacer()
+            
+            Text("\(notes.count)")
+        } //: VSTACK
+        .navigationTitle("Notes")
     }
 }
 
+// MARK: - PREVIEW
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
